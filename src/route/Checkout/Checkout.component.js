8cb699c26b10eb8c8b94dd4a2ed42faa8eee9584
igor2000xp <igorring@gmail.com>
2022-11-22 20:55:34 +0300

    fix: add Checkout.style.scss to override

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -2,6 +2,8 @@ import {
     Checkout as SourceCheckout,
 } from 'SourceRoute/Checkout/Checkout.component';
 
+import './Checkout.style';
+
 /** @namespace ScandiTest/Route/Checkout/Component */
 export class CheckoutComponent extends SourceCheckout {
     // TODO implement logic
