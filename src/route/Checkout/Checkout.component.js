7cabbde9e39aa45b2ba736b94e53782c93719cf4
igor2000xp <igorring@gmail.com>
2022-11-22 19:51:13 +0300

    feat: override route Checkout

---
+++ src/route/Checkout/Checkout.component.js
@@ -0,0 +1,10 @@
+import {
+    Checkout as SourceCheckout,
+} from 'SourceRoute/Checkout/Checkout.component';
+
+/** @namespace ScandiTest/Route/Checkout/Component */
+export class CheckoutComponent extends SourceCheckout {
+    // TODO implement logic
+}
+
+export default CheckoutComponent;
